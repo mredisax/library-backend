ca4c88a0c3fedfe7c36994164bbd3c859947beec
Patryk <apatryk19@gmail.com>
2023-11-20 18:14:26 +0100

    books CRUD

--- src/book/book.entity.ts
+++ src/book/book.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
 import { Author } from '../author/author.entity';
 @Entity()
 
---
+++ src/book/dto/create-book.dto.ts
@@ -0,0 +1,11 @@
+
+export class CreateBookDto {
+    
+    readonly title: string;
+    readonly description: string;
+    readonly category: string;
+    readonly year: number;
+    readonly isbn: string;
+    readonly author: number;
+
+}
